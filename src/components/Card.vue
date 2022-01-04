e6a43c2ddaa83ac05a33daaed5da1cc2ebc562c7
todelusion <todelusion@gmail.com>
2022-01-04 20:58:06 +0800

    start coding card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,14 @@
 <template>
-<body class="bg-[#fff]">
-    <h1>test</h1>
+<body class="bg-[#E5E5E5]">
+    <div class="p-4 h-screen lg:p-8">
+        <div class="flex justify-between items-start lg:max-w-[1280px] lg:mx-auto">
+            <div class="border-gray-500">
+                <h1 class="text-4xl text-gray-800 tracking-wide">Start now</h1>
+                <p class="text-4xl text-gray-800 tracking-wide">your <span class="font-bold">free plan</span></p>
+            </div>
+            <button class="bg-primary text-white py-2 px-4 tracking-widest rounded-sm ring-primary ring-offset-2 active:ring-2 duration-100">LOGIN</button>    
+        </div>
+
+    </div>
 </body>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#E9175E'
+      }, 
+    },
   },
   variants: {
     extend: {},

a1ffdd37cbac57efebfa175dc88d504dbf1c4b13
todelusion <todelusion.gmail.com>
2022-01-18 23:53:06 +0800

    2022.01.18_component插入練習

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,11 +13,15 @@
             <p class="text-3xl font-bold">Free $0 <span class="text-base text-gray-600 font-medium">per mounth</span></p>
             <div class="w-[30px] h-[3px] bg-primary mt-4"></div>
             <p class="mt-8 font-bold text-lg">Plan includes:</p>
-            <ul id="list1" class="text-lg mb-20">
+
+            <!-- 未使用v-for -->
+            <!-- <ul id="list1" class="text-lg mb-20">
                 <li class="flex items-center mt-6">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                     </svg>
+
+                    
                     item item item item 
                 </li>
                  <li class="flex items-center mt-6">
@@ -32,6 +36,11 @@
                     </svg>
                     item item item item 
                 </li>
+            </ul> -->
+             
+             <!-- 嘗試使用v-for -->
+             <ul id="list1" class="text-lg mb-20">
+            <Clist /><Clist /><Clist />
             </ul>
 
             <a href="#" class="block w-full mb-8 font-bold text-center">view all the features</a>
@@ -43,20 +52,20 @@
 </template>
 
 <script>
+import Clist from './Clist.vue'
 export default {
     data(){
         return{
             lists:[
-                { 
-                    item: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-                    </svg>
-                    item item item item ` 
-                    
-                    }
+                {title: 'list1'},
+                {title: 'list2'},
+                {title: 'list3'}
             ]
         }
 
+    },
+    components: {
+        Clist
     }
 }
 </script>

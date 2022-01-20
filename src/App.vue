77ae4fa34bed5b3896e7cc762d151835d29b52ef
todelusion <todelusion.gmail.com>
2022-01-20 22:36:31 +0800

    2022.01.20_Card done

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,71 +1,92 @@
 <template>
-<body class="bg-[#E5E5E5]">
-    <div class="p-4 h-screen lg:p-8">
-        <div class="flex justify-between items-start lg:max-w-[1280px] lg:mx-auto">
-            <div class="border-gray-500">
-                <h1 class="text-4xl text-gray-800 tracking-wide">Start now</h1>
-                <p class="text-4xl text-gray-800 tracking-wide">your <span class="font-bold">free plan</span></p>
+
+    <body class="bg-[#F5F5F5] min-h-screen">
+        <div class="pt-5 px-16 lg:p-8">
+            <div class="flex justify-between items-start lg:max-w-[1280px] lg:mx-auto">
+                <div class="border-gray-500">
+                    <h1 class="text-4xl text-gray-800 tracking-wide">Start now</h1>
+                    <p class="text-4xl text-gray-800 tracking-wide">your <span class="font-bold">free plan</span></p>
+                </div>
+                <button
+                    class="bg-primary text-white py-2 px-4 tracking-widest rounded-sm ring-primary ring-offset-2 active:ring-2 duration-100">LOGIN</button>
             </div>
-            <button class="bg-primary text-white py-2 px-4 tracking-widest rounded-sm ring-primary ring-offset-2 active:ring-2 duration-100">LOGIN</button>    
+            <div class="lg:grid lg:grid-cols-3 lg:justify-evenly lg:gap-8 lg:max-w-[1280px] lg:mx-auto">
+                <div class="w-full p-8 mt-16 bg-secondary rounded-2xl hover:scale-105 duration-150 shadow-2xl">
+                    <p class="text-3xl font-bold">Free $0 <span class="text-base text-gray-600 font-medium">per
+                            mounth</span></p>
+                    <div class="w-[30px] h-[3px] bg-primary mt-4"></div>
+                    <p class="mt-8 font-bold text-lg">Plan includes:</p>
+
+                    <!-- 嘗試使用v-for -->
+                    <ul id="list1" class="text-lg mb-20">
+                        <Clist />
+                        <Clist />
+                        <Clist />
+                    </ul>
+
+                    <a href="#" class="block w-full mb-8 font-bold text-center">view all the features</a>
+                    <button
+                        class="w-full bg-primary py-3 text-white rounded-lg tracking-wider text-lg">subscribe</button>
+                </div>
+                <div class="w-full p-8 mt-16 bg-primary rounded-2xl hover:scale-105 duration-150 text-white shadow-2xl">
+                    <p class="text-3xl font-bold">Standard 20$ <span class="text-base text-white font-medium">per
+                            mounth</span></p>
+                    <div class="w-[30px] h-[3px] bg-white mt-4"></div>
+                    <p class="mt-8 font-bold text-lg">Plan includes:</p>
+
+                    <!-- 嘗試使用v-for -->
+                    <ul id="list1" class="text-lg mb-20">
+                        <Clist color="text-white" />
+                        <Clist color="text-white" />
+                        <Clist color="text-white" />
+                    </ul>
+
+                    <a href="#" class="block w-full mb-8 font-bold text-center">view all the features</a>
+                    <button
+                        class="w-full bg-white py-3 text-black rounded-lg tracking-wider text-lg">subscribe</button>
+                </div>
+                <div class="w-full p-8 mt-16 bg-secondary rounded-2xl hover:scale-105 duration-150 shadow-2xl">
+                    <p class="text-3xl font-bold">Pro $90 <span class="text-base text-gray-600 font-medium">per
+                            mounth</span></p>
+                    <div class="w-[30px] h-[3px] bg-primary mt-4"></div>
+                    <p class="mt-8 font-bold text-lg">Plan includes:</p>
+
+                    <!-- 嘗試使用v-for -->
+                    <ul id="list1" class="text-lg mb-20">
+                        <Clist />
+                        <Clist />
+                        <Clist />
+                    </ul>
+
+                    <a href="#" class="block w-full mb-8 font-bold text-center">view all the features</a>
+                    <button
+                        class="w-full bg-primary py-3 text-white rounded-lg tracking-wider text-lg">subscribe</button>
+                </div>
+            </div>
         </div>
-    <div class="mt-8  lg:max-w-[1280px] lg:mx-auto">
-        <div class="w-full p-8 mt-8 bg-secondary rounded-2xl">
-            <p class="text-3xl font-bold">Free $0 <span class="text-base text-gray-600 font-medium">per mounth</span></p>
-            <div class="w-[30px] h-[3px] bg-primary mt-4"></div>
-            <p class="mt-8 font-bold text-lg">Plan includes:</p>
-
-            <!-- 未使用v-for -->
-            <!-- <ul id="list1" class="text-lg mb-20">
-                <li class="flex items-center mt-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-                    </svg>
-
-                    
-                    item item item item 
-                </li>
-                 <li class="flex items-center mt-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-                    </svg>
-                    item item item item 
-                </li>
-                 <li class="flex items-center mt-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-4 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-                    </svg>
-                    item item item item 
-                </li>
-            </ul> -->
-             
-             <!-- 嘗試使用v-for -->
-             <ul id="list1" class="text-lg mb-20">
-            <Clist /><Clist /><Clist />
-            </ul>
-
-            <a href="#" class="block w-full mb-8 font-bold text-center">view all the features</a>
-            <button class="w-full bg-primary py-3 text-white rounded-lg tracking-wider text-lg">subscribe</button>
-        </div>
-    </div>
-    </div>
-</body>
+    </body>
 </template>
 
 <script>
-import Clist from './Clist.vue'
-export default {
-    data(){
-        return{
-            lists:[
-                {title: 'list1'},
-                {title: 'list2'},
-                {title: 'list3'}
-            ]
-        }
+    import Clist from './Clist.vue'
+    export default {
+        data() {
+            return {
+                lists: [{
+                        title: 'list1'
+                    },
+                    {
+                        title: 'list2'
+                    },
+                    {
+                        title: 'list3'
+                    }
+                ]
+            }
 
-    },
-    components: {
-        Clist
+        },
+        components: {
+            Clist
+        }
     }
-}
 </script>
